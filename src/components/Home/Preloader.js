d0a1b442ed7a62861d5661aaf9cacd71fe9fc2e0
defk0n1 <zrigua.ghassen@gmail.com>
2024-08-23 11:21:29 +0200

    added position sticky to preloader

--- src/components/Home/Preloader.js
+++ src/components/Home/Preloader.js
@@ -3,8 +3,8 @@ import loadergif from '../../assets/loading-icon.gif'
 
 const Preloader = () => {
   return (
-    <div style={{height:"100vh" , width:"100vw" , display:"flex" , justifyContent:"center" , alignItems:"center" ,backgroundColor:"black"
-    ,zIndex:"1999"}}><img height="300" width="300" src={loadergif}/></div>
+    <div style={{height:"100vh" , width:"100vw"  , justifyContent:"center" , alignItems:"center" ,backgroundColor:"black"
+    ,zIndex:"1999",display:"flex", position:"sticky",top:"0px"}}><img height="300" width="300" src={loadergif}/></div>
   )
 }
 
